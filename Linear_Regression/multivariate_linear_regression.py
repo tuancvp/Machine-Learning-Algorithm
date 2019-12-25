1ff11c44595281e139da1720e2f2ebe223d35eb7
tuanvision <tuannm0312@gmail.com>
2019-12-25 17:47:21 +0700

    fix MLR

--- Linear_Regression/multivariate_linear_regression.py
+++ Linear_Regression/multivariate_linear_regression.py
@@ -8,8 +8,8 @@ print(__doc__)
 """
 Dummy data for Multivariate Regression
 """
-X_train = np.array([[1, 2, 3, 4, 5, 6, 7, 8]]).T
-Y_train = np.array([[1, 2, 4, 3, 5.5, 8, 6, 8.4]]).T
+X_train = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 5]]).T
+Y_train = np.array([[1, 2, 4, 3, 5.5, 8, 6, 8.4, 10, 4]]).T
 
 
 """
@@ -17,10 +17,12 @@ Hypothesis Function
 """
 def h_function(x, theta):
 	# theta(k, 1) x(k, 1)
-	# print(theta.shape, x.shape)
+	# print(theta.shape, x.T.shape)
 	# x = np.array(x).reshape(-1, 1)
 	# print(theta.T, x)
-	return np.matmul(theta.T, x)[0][0]
+	# print("MAT: ", np.matmul(theta, x.T)[0][0])
+	# exit(0)
+	return np.matmul(theta, x.T)[0][0]
 
 """
 Update features by order of the regression
@@ -43,7 +45,10 @@ def loss_function_j(X_train, Y_train, theta, order_of_regression):
 	m = X_train.shape[0]
 	for (x, y) in zip(X_train, Y_train):
 		x_feature = update_features(x, order_of_regression)
+		# print("X_feature: {}".format(x_feature))
 		cost +=  (h_function(x_feature, theta) - y) ** 2
+		# print("cost: {} regular: {}".format(cost, (1.0 / (2.0 * m) ) * cost))
+	# exit(0)
 	return (1.0 / (2.0 * m) ) * cost
 
 """
@@ -56,34 +61,51 @@ def derivative_theta_i(X_train, Y_train, theta, order_of_regression, i):
 		# print(x, y)
 		x_feature = update_features(x, order_of_regression)
 		# print("x_feature: {}".format(x_feature))
-		result +=  (h_function(x_feature, theta) - y) * x_feature[0][i]
+		# print("h_function:", h_function(x_feature, theta) - y[0], y)
+		# loss = 
+		result +=  (h_function(x_feature, theta) - y[0]) * x_feature[0][i]
 		# print(h)
 		# print(x_feature, y)
 		# loss_j = loss_function_j(x, y) * x
-	return (1.0 / (2.0 * m) ) * result
+	# print("type result: ", type(result))
+
+	return (1.0 / m) * result
 
 
 def update_theta(X_train, Y_train, theta, alpha, order_of_regression):
 	temp = []
-	for i, old_theta in enumerate(theta):
+	print("theta: {}".format(len(theta)))
+
+	for i, old_theta in enumerate(theta[0]):
+
+		# print("old_theta: {} derivative_theta_i: {}".format(old_theta, derivative_theta_i(X_train, Y_train, theta, order_of_regression, i)))
+
 		new_theta_i = old_theta - alpha * derivative_theta_i(X_train, Y_train, theta, order_of_regression, i)
+		# print("new_theta_i: {}".format(new_theta_i))
 		temp.append(new_theta_i)
+
+	# print(np.atleast_2d(temp))
+	# exit(0)
 	return np.atleast_2d(temp)
 
 """
 Gradient Descent For Multivariate Regression
 """
 def gradient_descent(X_train, Y_train, tolerance = 0.001, alpha = 0.001, theta=[], order_of_regression=2):
-	if len(theta) == 0:
-		theta = np.atleast_2d(np.random.random(order_of_regression + 1) * 100.0)
+	# if len(theta) == 0:
+	# 	theta = np.atleast_2d(np.random.random(order_of_regression + 1))
 
-	print(theta.shape)
-	print(theta)
+	# print(theta.shape)
+	# print(theta)
+
+	theta = np.array([[0.89088266, 0.92116762, 0.53932061]])
+
 	loss_history = []
 	theta_history = []
-	cur_loss = 0.0
+	cur_loss = loss_function_j(X_train, Y_train, theta, order_of_regression)
 	prev_loss = 0.0
 	iter_number = 0
+	# return theta
 
 	while True:
 
@@ -92,11 +114,12 @@ def gradient_descent(X_train, Y_train, tolerance = 0.001, alpha = 0.001, theta=[
 		prev_loss = cur_loss
 		cur_loss = loss_function_j(X_train, Y_train, theta, order_of_regression)
 		iter_number += 1
-		# print("iter: {} prev_loss: {} cur_loss: {}".format(iter_number, prev_loss, cur_loss))
+		print("iter: {} prev_loss: {} cur_loss: {} theta: {}".format(iter_number, prev_loss, cur_loss, theta))
+		# break
 		if abs(cur_loss - prev_loss) < tolerance:
 			break
 
-
+	# print("cur_loss: {}".format(cur_loss))
 	return theta
 
 """
@@ -114,3 +137,36 @@ theta = gradient_descent(X_train, Y_train)
 # result = derivative_theta_i(X_train, Y_train, theta=np.array([[0.5, 0.2, 0.3]]), order_of_regression=2, i=1)
 # print(result)
 print(theta)
+
+
+	
+def prediction(X_train, theta, order_of_regression=2):
+	y_preds = []
+	for x in X_train:
+		x_feature = update_features(x, order_of_regression)
+		# print(value)	
+		y_pred = h_function(x_feature, theta)
+		print(x[0], x_feature, y_pred)
+		y_preds.append(y_pred)
+	
+	print(y_preds)
+
+	return y_preds
+
+import matplotlib.pyplot as plt
+
+# x = np.array(data).T[0]
+# y = np.array(data).T[1]
+x = X_train.reshape(-1, 1)
+y = Y_train.reshape(-1, 1)
+
+print(X_train.shape)
+y_preds = prediction(X_train, theta)
+# x, y = zip(*np.array(X_train))
+
+# y_preds = prediction(x, theta)
+# # print(x, y)
+plt.scatter(x, y, color='red')
+plt.scatter(x, y_preds, color='blue')
+
+plt.show()
--- Linear_Regression/ori_MLR.py
+++ Linear_Regression/ori_MLR.py
@@ -12,82 +12,131 @@ data = [(1, 1), (2, 2), (3, 4), (4, 3), (5, 5.5), (6, 8), (7, 6), (8, 8.4), (9, 
 Plot the line using theta_values
 """
 def plot_line(formula, x_range, order_of_regression):
-    x = np.array(x_range).tolist()  
-    y = [formula(update_features(x_i, order_of_regression)) for x_i in x]
-    plt.plot(x, y)
+	x = np.array(x_range).tolist()  
+	y = [formula(update_features(x_i, order_of_regression)) for x_i in x]
+	plt.plot(x, y)
 
 """
 Hypothesis Function
 """
 def h(x, theta):
-    return np.matmul(theta.T, x)[0][0]
+	# print(theta.T.shape, x.shape)
+	return np.matmul(theta.T, x)[0][0]
 
 """
 Partial Derivative w.r.t. theta_i
 """
 def j_prime_theta(data, theta, order_of_regression, i):
-    result = 0
-    m = len(data)
-    for x, y in data :
-        x = update_features(x, order_of_regression)
-        result += (h(x, theta) - y) * x[i]
-    
-    return (1/m) * result
+	result = 0
+	m = len(data)
+	for x, y in data :
+		x = update_features(x, order_of_regression)
+		result += (h(x, theta) - y) * x[i]
+	
+	return (1/m) * result
 
 """
 Update features by order of the regression
 """
 
 def update_features(x, order_of_regression):
-    features = [1]
-    for i in range(order_of_regression):
-        features.append(math.pow(x, i+1))
-    return np.atleast_2d(features).T
+	features = [1]
+	for i in range(order_of_regression):
+		features.append(math.pow(x, i+1))
+	return np.atleast_2d(features).T
 
 """
 Cost Function
 """
 def j(data, theta, order_of_regression):
-    cost = 0
-    m = len(data)
-    for x, y in data:
-        x = update_features(x, order_of_regression)
-        cost += math.pow(h(x, theta) - y, 2)
-    return (1/(2*m)) * cost
+	cost = 0
+	m = len(data)
+	for x, y in data:
+		x = update_features(x, order_of_regression)
+		# print("jF: x = {}".format(x))
+		# print("h function: {}".format(h(x, theta)))
+		cost += math.pow(h(x, theta) - y, 2)
+		# print("cost: {} regu: {}".format(cost, (1/(2*m)) * cost))
+	# exit(0)
+	return (1/(2*m)) * cost
+
 
 """
 Simultaneous Update
 """
 def update_theta(data, alpha, theta, order_of_regression):
-    temp = []
-    for i in range(order_of_regression+1):
-        temp.append(theta[i] - alpha * j_prime_theta(data, theta, order_of_regression, i))
-    theta = np.array(temp)
-    return theta
-    
+	temp = []
+	for i in range(order_of_regression+1):
+		# print("j_prime_theta: {}".format(j_prime_theta(data, theta, order_of_regression, i)))
+		temp.append(theta[i] - alpha * j_prime_theta(data, theta, order_of_regression, i))
+		# print("new theta i: ", theta[i] - alpha * j_prime_theta(data, theta, order_of_regression, i))
+	# exit(0)
+	theta = np.array(temp)
+	return theta
+	
 """
 Gradient Descent For Multivariate Regression
 """
 def gradient_descent(data, alpha, tolerance, theta=[], order_of_regression = 2):
-    if len(theta) == 0:
-        theta = np.atleast_2d(np.random.random(order_of_regression+1) * 100).T
-    prev_j = 10000
-    curr_j = j(data, theta, order_of_regression)
-    # print(curr_j)
-    cost_history = []
-    theta_history = [] 
-    while(abs(curr_j - prev_j) > tolerance):
-        try:
-            cost_history.append(curr_j)
-            theta_history.append(theta)
-            theta = update_theta(data, alpha, theta, order_of_regression)
-            prev_j = curr_j
-            curr_j = j(data, theta, order_of_regression)
-            # print(curr_j)
-        except:
-            break
-    # print("Stopped with Error at %.5f" % prev_j)
-    return theta
+	# if len(theta) == 0:
+	# 	theta = np.atleast_2d(np.random.random(order_of_regression+1) * 1).T
+
+	theta = np.array([[0.89088266], [0.92116762], [0.53932061]])
+
+	prev_j = 10000
+
+	curr_j = j(data, theta, order_of_regression)
+	# print(curr_j)
+	cost_history = []
+	theta_history = [] 
+	iter = 0
+	# print("START")
+	while(abs(curr_j - prev_j) > tolerance):
+		# print("iter: {} loss: {} {}".format(iter, curr_j, prev_j))
+
+		try:
+			cost_history.append(curr_j)
+			theta_history.append(theta)
+			# print(curr_j, theta)
+			theta = update_theta(data, alpha, theta, order_of_regression)
+			prev_j = curr_j
+			# print("IN TRY")
+			curr_j = j(data, theta, order_of_regression)
+			iter += 1
+
+			print("iter: {} loss: {} {} {}".format(iter, curr_j, prev_j, theta))
+			# break
+		except:
+			break
+	# print("Stopped with Error at %.5f" % prev_j)
+	return theta
 
 theta = gradient_descent(data, 0.001, 0.001)
 print(theta)
+
+# plot_line(theta, data, 2)
+
+def prediction(x, theta):
+	y_preds = []
+	for value in x:
+		value = update_features(value, order_of_regression=2)
+		# print(value)
+		y_pred = h(value, theta)
+		# print(value, y_pred)
+		y_preds.append(y_pred)
+	return y_preds
+
+x = np.array(data).T[0]
+y = np.array(data).T[1]
+x, y = zip(*np.array(data))
+
+y_preds = prediction(x, theta)
+# print(x, y)
+plt.scatter(x, y, color='red')
+plt.scatter(x, y_preds, color='blue')
+
+plt.show()
+# plt.plot(x, y)
+# print(x)
+# print(y)
+# plt.show()
